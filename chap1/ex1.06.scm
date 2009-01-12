047e9f3426ae36638b075cdcd940ee675370887b
Desmond Brand <dmnd@desmondbrand.com>
2009-01-12 23:03:44 +1100

    Finished 1.1

---
+++ chap1/ex1.06.scm
@@ -0,0 +1,11 @@
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+;; new-if won't work correctly because the clauses will both be evaulated due to applicative order.
